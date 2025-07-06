5d3e3ddac020d2c3b7eadd0a1b687b63ef137a20
ektagoel-12 <ektagoel12july@gmail.com>
2025-07-06 22:50:51 +0530

    Initial commit

---
+++ financial_agent.py
@@ -0,0 +1,44 @@
+from phi.agent import Agent
+from phi.model.groq import Groq
+from phi.tools.yfinance import YFinanceTools
+from phi.tools.duckduckgo import DuckDuckGo
+import openai
+
+import os
+from dotenv import load_dotenv
+load_dotenv()
+openai.api_key=os.getenv("OPENAI_API_KEY")
+## web search agent
+web_search_agent=Agent(
+    name="Web search Agent",
+    role="Searchthe web for the information",
+    model=Groq(id="llama3-70b-8192"),
+    tools=[DuckDuckGo()],
+    instructions=["Always include sources"],
+    show_tool_calls=True,
+    markdown=True,
+
+)
+
+## Financial agent
+finance_agent=Agent(
+    name="Finance AI Agent",
+    model=Groq(id="llama3-70b-8192"),
+    tools=[
+        YFinanceTools(stock_price=True, analyst_recommendations=True, company_info=True, company_news=True)
+    ],
+    instructions=["Use tables to display the data"],
+    show_tool_calls=True,
+    markdown=True,
+)
+
+multi_ai_agent=Agent(
+    model=Groq(id="llama3-70b-8192"),
+    team=[web_search_agent,finance_agent],
+    instructions=["Always include sources","Use tableto display the data"],
+    show_tool_calls=True,
+    markdown=True,
+)
+
+multi_ai_agent.print_response("Summarize analyst recommendation and share the latest newsfor NVIDIA",stream=True)
+
